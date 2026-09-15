9cf11996c6f99f68eea8418e13976dc21b351c63
Blockhead4103 <kevin.lancashire@gmail.com>
2026-09-15 15:59:14 +0200

    Update import path for ArchitectureAdvisorForm

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import ArchitectureAdvisorForm from "./components/ArchitectureAdvisorForm";
+import { ArchitectureAdvisorForm } from '@/app/components/ArchitectureAdvisorForm';
 
 export default function Home() {
   return (
